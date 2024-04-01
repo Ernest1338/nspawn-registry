d3535783196479d0018f52fc94008d0bb5d23257
Ernest Gupik <ernestgupik@wp.pl>
2024-04-01 22:22:13 +0200

    nspawn-cli: implement image pull

--- nspawn-cli.py
+++ nspawn-cli.py
@@ -3,29 +3,72 @@
 import argparse
 import json
 import os
+import tarfile
+import shutil
 
 import urllib.request
 
-# urllib.request.urlopen
-# urllib.request.urlretrieve
+# TODO: handle image versions
 
 HOME = os.path.expanduser("~")
 IMAGES_SAVE_DIR = os.path.join(HOME, ".local", "share", "nspawn-images")
 
 
-def get_image_index():
-    return json.loads(
-        urllib.request.urlopen(
-            "https://raw.githubusercontent.com/Ernest1338/nspawn-registry/main/index.json"
+class ImageIndex:
+    def __init__(self):
+        self.index = json.loads(
+            urllib.request.urlopen(
+                # "https://raw.githubusercontent.com/Ernest1338/nspawn-registry/main/index.json"
+                "http://localhost:8080/index.json"
+            )
+            .read()
+            .decode()
         )
-        .read()
-        .decode()
-    )
+
+    def get_images(self):
+        return self.index
+
+    def get(self, image_name: str):
+        for image in self.index:
+            if image["name"] == image_name:
+                return image
+        return None
 
 
-def command_pull():
+def get_pulled_images():
+    images = []
+    for subdir, dirs, files in os.walk(IMAGES_SAVE_DIR):
+        for dir in dirs:
+            images.append(dir)
+    return images
+
+
+def command_pull(image_name: str):
     if not os.path.exists(IMAGES_SAVE_DIR):
         os.makedirs(IMAGES_SAVE_DIR)
+
+    index = ImageIndex()
+    image = index.get(image_name)
+
+    if image is None:
+        print(f"[!] ERROR: Image not found: {image_name}")
+        exit(1)
+
+    pull_path = os.path.join(IMAGES_SAVE_DIR, image_name)
+
+    if os.path.exists(pull_path):
+        print("[!] Image already exists. Updating...")
+        shutil.rmtree(pull_path)
+
+    print(f"[+] Pulling image: '{image_name}' into '{IMAGES_SAVE_DIR}/{image_name}'")
+
+    try:
+        retrieved_image = urllib.request.urlretrieve(image["url"])[0]
+        image_tar = tarfile.open(retrieved_image)
+        image_tar.extractall(pull_path)
+        os.remove(retrieved_image)
+    except:
+        print("[!] ERROR: Retrieving image")
 
 
 def command_new():
@@ -33,11 +76,13 @@ def command_new():
 
 
 def command_list():
-    images = get_image_index()
-    for image in images:
+    index = ImageIndex()
+    pulled_images = get_pulled_images()
+    for image in index.get_images():
         name = image["name"]
         version = image["version"]
-        print(f"{name} (v{version})")
+        pulled = " [pulled]" if name in pulled_images else ""
+        print(f"{name} (v{version}){pulled}")
 
 
 def main():
@@ -49,13 +94,16 @@ def main():
     subparsers = parser.add_subparsers(title="Subcommands", dest="subcommands")
 
     subcom_pull = subparsers.add_parser("pull")
+    subcom_pull.add_argument("image_name")
+
     subcom_new = subparsers.add_parser("new")
+
     subcom_list = subparsers.add_parser("list")
 
     args = parser.parse_args()
 
     if args.subcommands == "pull":
-        command_pull()
+        command_pull(args.image_name)
     elif args.subcommands == "new":
         command_new()
     elif args.subcommands == "list":
